bbc5a205780edc63f273807e418d99bcf27455ae
Bipul anand <136790639+bipulTutor@users.noreply.github.com>
2023-11-26 23:37:39 +0530

    Add files via upload

---
+++ Exception Handling1.py
@@ -0,0 +1,9 @@
+a = input("Enter the number: ")
+print(f"Multiplication table of {a} is:")
+
+for i in range(1, 11):
+  print(f"{int(a)} X {i} = {int(a)*i}")
+
+
+print("Some lines of code")
+print("End of program")
